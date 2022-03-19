8facc0396cb91260259e47969fe31a7f8f30c129
Isaias Carrion <carrionisaias@gmail.com>
2022-03-19 12:29:28 -0300

    testing

--- archBased/nvim/plug-config/emmet.vim
+++
@@ -1,2 +0,0 @@
-"emmet leader
-let g:user_emmet_leader_key=','
--- debianBased/update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-patching(){
-    sudo apt-get update -y;
-    sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y;
-};
-
-patching;
